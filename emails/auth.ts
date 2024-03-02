1f61db4a17900b4801e27c852f963b26da0b5f1b
Matias Andrada <matiasandrada5622@gmail.com>
2024-03-02 04:47:53 -0300

    Añadir configuración de correo electrónico para enviar tokens de autenticación y restablecimiento de contraseña

--- emails/auth.ts
+++ emails/auth.ts
@@ -1,56 +1,44 @@
-import { Resend } from "resend";
+"use server";
+import { transporter } from "@/emails/client";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const domain = process.env.APP_URL || "http://localhost:3000";
+const fromEmail = (process.env.EMAIL_HOST as string) || " ";
 
-const domain = process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000";
-const fromEmail = process.env.EMAIL_AUTH || "auth@ProjectAdmin.com";
-
-export const sendTwoFactorTokenEmail = async (
-    email: string,
-    token: string
-) => {
+export const sendTwoFactorTokenEmail = async (email: string, token: string) => {
     try {
-        await resend.emails.send({
+        await transporter.sendMail({
             from: fromEmail,
             to: email,
             subject: "2FA Code",
-            html: `<p>Your 2FA code: ${token}</p>`
+            html: `<p>Your 2FA code: ${token}</p>`,
         });
     } catch (error) {
         console.error("Error sending 2FA token email:", error);
     }
 };
 
-export const sendPasswordResetEmail = async (
-    email: string,
-    token: string,
-) => {
+export const sendPasswordResetEmail = async (email: string, token: string) => {
     try {
-        const resetLink = `${domain}/auth/new-password?token=${token}`
-
-        await resend.emails.send({
+        const resetLink = `${domain}/auth/new-password?token=${token}`;
+        await transporter.sendMail({
             from: fromEmail,
             to: email,
             subject: "Reset your password",
-            html: `<p>Click <a href="${resetLink}">here</a> to reset password.</p>`
+            html: `<p>Click <a href="${resetLink}">here</a> to reset password.</p>`,
         });
     } catch (error) {
         console.error("Error sending password reset email:", error);
     }
 };
 
-export const sendVerificationEmail = async (
-    email: string,
-    token: string
-) => {
+export const sendVerificationEmail = async (email: string, token: string) => {
     try {
         const confirmLink = `${domain}/auth/new-verification?token=${token}`;
-        await resend.emails.send({
+        await transporter.sendMail({
             from: fromEmail,
             to: email,
             subject: "Confirm your email",
-            html: `<p>Click <a href="${confirmLink}">here</a> to confirm email.</p>`
-
+            html: `<p>Click <a href="${confirmLink}">here</a> to confirm email.</p>`,
         });
     } catch (error) {
         console.error("Error sending verification email:", error);
---
+++ emails/client.js
@@ -0,0 +1,27 @@
+import nodemailer from "nodemailer";
+
+const adminEmail = process.env.ADMIN_EMAIL;
+const adminEmailFull = process.env.ADMIN_EMAIL_FULL;
+const pass = process.env.EMAIL_PASSWORD;
+
+export const transporter = nodemailer.createTransport({
+  host: process.env.EMAIL_HOST,
+  port: process.env.EMAIL_PORT,
+  secure: true,
+  auth: {
+    user: adminEmail,
+    pass,
+  },
+});
+
+try {
+  const testResult = transporter.verify();
+  console.log("Email service is ready to take our messages", testResult);
+} catch (error) {
+  console.error("Error verifying email service:", error);
+}
+
+export const mailOptions = {
+  from: adminEmailFull,
+  to: adminEmailFull,
+};
