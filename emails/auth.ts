f8dd46c586badc89935df3bb9e1731f9169bc54c
Matias Andrada <matiasandrada5622@gmail.com>
2024-03-02 02:49:48 -0300

    Actualizar configuración de correo electrónico y dependencias***
    ***Actualizar configuración de base de datos y dependencias***
    
    ***Traducción de mensaje de confirmación de commit: "Actualizar configuración de correo electrónico y dependencias"

--- emails/auth.ts
+++ emails/auth.ts
@@ -1,9 +1,9 @@
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(/* process.env.RESEND_API_KEY */);
 
-const domain = process.env.NEXT_PUBLIC_APP_URL;
-const fromEmail = process.env.EMAIL_AUTH || " ";
+const domain = ""/*"" process.env.NEXT_PUBLIC_APP_URL; */
+const fromEmail = ""/* process.env.EMAIL_AUTH || " "; */
 
 export const sendTwoFactorTokenEmail = async (
     email: string,
