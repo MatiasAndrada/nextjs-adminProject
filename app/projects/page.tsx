1989dda5bfb05683d97f7b05a547e9a581c952c4
Matias Andrada <matiasandrada5622@gmail.com>
2024-05-04 16:52:28 -0300

    Add avatar image and Fallback with initial of user

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,15 +1,14 @@
-/* import RevenueChart from "@/components/dashboard/revenue-chart";
-import LatestInvoices from "@/components/dashboard/latest-invoices"; */
 import { Suspense } from "react";
 import Link from "next/link";
-import { FaUser } from "react-icons/fa";
+/* import { FaUser } from "react-icons/fa"; */
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
 import { currentUser } from "@/hooks/use-current-user";
-import CardWrapper from '@/components/projects/cards';
-import { Button } from '@/components/ui/button';
+import CardWrapper from "@/components/projects/cards";
+import { Button } from "@/components/ui/button";
 import {
     /*     RevenueChartSkeleton,
-        LatestInvoicesSkeleton, */
-    CardsSkeleton
+          LatestInvoicesSkeleton, */
+    CardsSkeleton,
 } from "@/components/skeletons";
 import { lusitana } from "@/components/fonts";
 
@@ -18,26 +17,39 @@ export default async function Page() {
     return (
         <main>
             <div className="flex flex-row items-center justify-between">
-                <h1 className={`${lusitana.className} text-4xl `}>
-                    Projects page
-                </h1>
+                <h1 className={`${lusitana.className} text-4xl `}>Projects page</h1>
                 <div className="flex flex-row items-center justify-between md:space-x-4">
-                    <h2 className="text-lg font-medium 
+                    <h2
+                        className="text-lg font-medium 
                     md:text-xl
-                ">Hi!
+                "
+                    >
+                        Hi!
                         {user && ` ${user.name}`}
                     </h2>
-                    <FaUser className="w-8 h-8 " />
+                    <Avatar className="h-14 w-14">
+                        {/*  <AvatarImage src="https://www.shutterstock.com/image-vector/young-smiling-man-avatar-brown-600nw-2261401207.jpg" alt="John Doe" /> */}
+                        <AvatarFallback>PJ</AvatarFallback>
+                    </Avatar>
                 </div>
             </div>
             <div className="flex flex-row items-center justify-between">
                 <h2 className="text-lg font-medium ">Your projects</h2>
                 <Link href="/projects/create">
-                    <Button
-                        variant="create"
-                    >
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                    <Button variant="create">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="w-8 h-8"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                            />
                         </svg>
                         Create a new project
                     </Button>
