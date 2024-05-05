e894ab2149376c11d791912c78fec87aecc3182a
Matias Andrada <matiasandrada5622@gmail.com>
2024-05-05 20:06:41 -0300

    remove title page

--- app/(sidenav)/dashboard/tasks/page.tsx
+++ app/(sidenav)/dashboard/tasks/page.tsx
@@ -3,7 +3,6 @@ import Breadcrumbs from "@/components/breadcrumbs";
 import Search from "@/components/search";
 import Table from "@/components/tasks/table-head";
 import Pagination from "@/components/pagination";
-import { lusitana } from "@/components/fonts";
 import { CreateTask } from "@/components/tasks/buttons";
 import { fetch_task_pages } from "@/data/task";
 
@@ -27,17 +26,14 @@ export default async function Page({
 
     return (
         <div className="w-full">
-            <div className="flex flex-col gap-8 w-full items-start justify-between">
-                <Breadcrumbs
-                    breadcrumbs={[
-                        { label: "Dashboard", href: "/dashboard" },
-                        { label: "Tasks", href: "/dashboard/tasks", active: true },
-                    ]}
-                />
-            </div>
+            <Breadcrumbs
+                breadcrumbs={[
+                    { label: "Dashboard", href: "/dashboard" },
+                    { label: "Tasks", href: "/dashboard/tasks", active: true },
+                ]}
+            />
             <div>
-                <h1 className={`${lusitana.className} text-4xl mb-4`}>Tasks</h1>
-                <h2 className="text-xl font-bold">All task of project</h2>
+                <h2 className="text-xl font-base">All task of project</h2>
             </div>
             <div className="mt-4 flex items-center justify-between gap-2 mx-6">
                 <Search placeholder="Search task groups..." />
