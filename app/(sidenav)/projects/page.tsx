a73e9e92a002ed469005f9d90022f11adecac859
Matias Andrada <matiasandrada5622@gmail.com>
2024-05-06 17:09:59 -0300

    unused imports in page

--- app/(sidenav)/projects/page.tsx
+++ app/(sidenav)/projects/page.tsx
@@ -1,18 +1,5 @@
 import { Suspense } from "react";
 import Link from "next/link";
-/* import { FaUser } from "react-icons/fa"; */
-/* import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
-import {
-    DropdownMenu,
-    DropdownMenuTrigger,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuLabel,
-    DropdownMenuSeparator,
-    DropdownMenuGroup,
-    DropdownMenuPortal,
-} from "../../../components/ui/dropdown-menu" */
-import { currentUser } from "@/hooks/use-current-user";
 import CardWrapper from "@/components/projects/cards";
 import { Button } from "@/components/ui/button";
 import {
@@ -23,46 +10,10 @@ import {
 import { lusitana } from "@/components/fonts";
 
 export default async function Page() {
-    const user = await currentUser();
-    /*     const initialLetters = user?.name?.split(" ").map((n) => n[0]) ?? ""; */
     return (
         <main>
             <div className="flex flex-row items-center justify-between">
                 <h1 className={`${lusitana.className} text-4xl `}>Projects page</h1>
-                <div className="flex flex-row items-center justify-between md:space-x-4">
-                    {/*                     <h2
-                        className="text-lg font-medium 
-                    md:text-xl
-                "
-                    >
-                        Hi!
-                        {user && ` ${user.name}`}
-                    </h2> */}
-                    {/*                     <DropdownMenu >
-                        <DropdownMenuTrigger>
-                            <Avatar className="h-14 w-14">
-                                {user && user.image ? (
-                                    <AvatarImage src={user.image} alt="Icon user" />
-                                ) : (
-                                    <AvatarFallback>{initialLetters}</AvatarFallback>
-                                )}
-                            </Avatar>
-                        </DropdownMenuTrigger>
-                        <DropdownMenuPortal >
-                            <DropdownMenuContent className="mr-4">
-                                <DropdownMenuLabel className="text-green-500">Free plan</DropdownMenuLabel>
-                                <DropdownMenuLabel className="text-sm font-light">{user?.email}</DropdownMenuLabel>
-                                <DropdownMenuSeparator className="my-2 bg-slate-300" />
-                                <DropdownMenuGroup>
-                                    <DropdownMenuItem>Profile</DropdownMenuItem>
-                                    <DropdownMenuItem>Settings</DropdownMenuItem>
-                                </DropdownMenuGroup>
-                                <DropdownMenuSeparator className="my-4 bg-slate-300" />
-                                <DropdownMenuItem>Sign out</DropdownMenuItem>
-                            </DropdownMenuContent>
-                        </DropdownMenuPortal>
-                    </DropdownMenu> */}
-                </div>
             </div>
             <div className="flex flex-row items-center justify-between">
                 <h2 className="text-lg font-medium ">Your projects</h2>
