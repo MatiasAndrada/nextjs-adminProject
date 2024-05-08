df634ba7b11d1456b27b6b7dd02b0968731dc312
Matias Andrada <matiasandrada5622@gmail.com>
2024-05-07 22:31:17 -0300

    fix warning nested layout with multiples <html>

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -17,29 +17,24 @@ export const metadata: Metadata = {
 export default async function Layout({ children }: { children: React.ReactNode }) {
   const project = await currentProject();
   return (
-    <html lang="en">
-      <body className={`${inter.className} antialiased bg-slate-200 dark:bg-slate-950 text-black dark:text-white`} id="main_layout">
-        <CurrentProjectStoreProvider>
-          <div className=" h-screen flex flex-col md:flex-row md:overflow-hidden">
-            <div className="w-full flex-none md:w-64">
-              <SideNav />
-            </div>
-            {/*       {      <div className="p-2 md:overflow-y-auto md:p-4">{children}</div>} */}
-            {project === null ? (
-              <div className="flex items-center justify-center h-screen w-full mx-auto">
-                <h3 className="text-2xl text-red-500 h-32">
-                  First you must select a project!
-                </h3>
-              </div>
-
-            ) : (
-              /*         <div className="w-full flex-grow px-4 md:overflow-y-auto md:px-6">{children}</div> //con padding*/
-              <div className="h-screen flex-grow md:overflow-y-auto  p-6 md:p-12">{children}</div>
-            )}
+    <body className={`${inter.className} antialiased bg-slate-200 dark:bg-slate-950 text-black dark:text-white`} id="main_layout">
+      <CurrentProjectStoreProvider>
+        <div className=" h-screen flex flex-col md:flex-row md:overflow-hidden">
+          <div className="w-full flex-none md:w-64">
+            <SideNav />
           </div>
-        </CurrentProjectStoreProvider>
-      </body>
-    </html>
-
+          {project === null ? (
+            <div className="flex items-center justify-center h-screen w-full mx-auto">
+              <h3 className="text-2xl text-red-500 h-32">
+                First you must select a project!
+              </h3>
+            </div>
+          ) : (
+            /*         <div className="w-full flex-grow px-4 md:overflow-y-auto md:px-6">{children}</div> //con padding*/
+            <div className="h-screen flex-grow md:overflow-y-auto  p-6 md:p-12">{children}</div>
+          )}
+        </div>
+      </CurrentProjectStoreProvider>
+    </body>
   );
 }
--- app/projects/layout.tsx
+++ app/projects/layout.tsx
@@ -5,20 +5,17 @@ import { CurrentProjectStoreProvider } from "@/context/currentProjectStore";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} antialiased bg-slate-200 dark:bg-slate-950 text-black dark:text-white`} id="main_layout">
-
-        <CurrentProjectStoreProvider>
-          <div className="md:overflow-hidden">
-            <div className="flex flex-col md:flex-row">
-              <div className="w-full flex-none md:w-64">
-                <SideNav />
-              </div>
-              <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+    <body className={`${inter.className} antialiased bg-slate-200 dark:bg-slate-950 text-black dark:text-white`} id="main_layout">
+      <CurrentProjectStoreProvider>
+        <div className="md:overflow-hidden">
+          <div className="flex flex-col md:flex-row">
+            <div className="w-full flex-none md:w-64">
+              <SideNav />
             </div>
+            <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
           </div>
-        </CurrentProjectStoreProvider>
-      </body>
-    </html>
+        </div>
+      </CurrentProjectStoreProvider>
+    </body>
   );
 }
