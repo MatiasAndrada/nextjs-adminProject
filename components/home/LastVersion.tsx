506e117f67f53d4d2fb4169c3b023859efea01a8
Matias Andrada <matiasandrada5622@gmail.com>
2024-05-14 15:27:37 -0300

    finished implementation

--- components/home/LastVersion.tsx
+++ components/home/LastVersion.tsx
@@ -1,10 +1,29 @@
 import React from 'react'
 
-const LastVersion = () => {
-    const res = fetch('https://api.github.com/repos/matiasandrada/nextjs-adminProject/commits')
-
+async function LastVersion() {
+    const response = await fetch('https://api.github.com/repos/matiasandrada/nextjs-adminProject/commits').then(res => res.json()).catch(err => console.error(err))
+    const lastCommitAuthor = response[0]?.commit.author.name
+    console.log("🦇  LastVersion  lastCommitAuthor:", lastCommitAuthor)
+    const lastCommitDate = response[0]?.commit.author.date
+    console.log("🦇  LastVersion  lastCommitDate:", lastCommitDate)
+    const transformDate = new Date(lastCommitDate).toLocaleDateString(undefined, { day: 'numeric', month: 'long', year: 'numeric' })
+    if (!response) return <div>loading...</div>
+    if (!lastCommitAuthor && lastCommitDate === undefined) {
+        const resMessage = response.message
+        const extractIP = resMessage?.split(' ')[5]
+        return (
+            <div className='text-gray-500 text-sm opacity-60'>
+                <h4 className='w-36 text-balance'>Exceded IP limit request in API github</h4>
+                <p>IP: {extractIP}</p>
+            </div>
+        )
+    }
     return (
-        <div>LastVersion</div>
+        <div className='text-gray-500 text-sm opacity-60'>
+            <h4>Ultima actualización</h4>
+            <p>Autor: {lastCommitAuthor}</p>
+            <p>Fecha: {transformDate}</p>
+        </div>
     )
 }
 
---
+++ components/home/StatusSession.tsx
@@ -0,0 +1,19 @@
+import { SessionClient } from "@/components/home/SessionClient";
+import { SessionServer } from "@/components/home/SessionServer";
+
+
+export function StatusSession() {
+    return (
+        <div className="opacity-60">
+            <h4 className="text-gray-500 text-balance text-sm w-36">
+                Verify the server and client session status:
+            </h4>
+            <div className="flex flex-row gap-2">
+                <span className="text-xs">Server:</span>
+                <SessionServer />
+                <span className="text-xs">Client:</span>
+                <SessionClient />
+            </div>
+        </div >
+    );
+}
