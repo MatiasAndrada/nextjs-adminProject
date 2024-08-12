fa25bc1e30f83263e60fef904e98a2f86ef9992f
Matias Andrada <matiasandrada5622@gmail.com>
2024-08-12 16:16:41 -0300

    enable edge

--- app/(sidenav)/dashboard/members/edit/[...ids]/not-found.tsx
+++ app/(sidenav)/dashboard/members/edit/[...ids]/not-found.tsx
@@ -1,9 +1,3 @@
-import React from 'react'
-
-const notFound = () => {
-    return (
-        <div>not-found</div>
-    )
+export default function notFound() {
+  return <div>not-found</div>;
 }
-
-export default notFound
