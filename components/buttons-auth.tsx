fe2b6f210f85c30dcf0075472e640343b8f6f43a
Matias Andrada <matiasandrada5622@gmail.com>
2024-08-13 17:04:18 -0300

    changes for more reusability

--- components/buttons-auth.tsx
+++ components/buttons-auth.tsx
@@ -1,35 +1,45 @@
 "use client";
 import { Button } from "./ui/button";
 import { signIn, signOut } from "next-auth/react";
-import { ArrowRightIcon } from "@heroicons/react/24/outline";
+import { ArrowRightIcon, PowerIcon } from "@heroicons/react/24/outline";
 
 import Link from "next/link";
 
 export const LoginButton = () => {
-    return (
-        <Button onClick={() => signIn()} size={"lg"}>
-            <span className="mr-2">Sign In</span> <ArrowRightIcon className="w-5 md:w-6" />
-        </Button>
-    );
+  return (
+    <Button onClick={() => signIn()} size={"lg"}>
+      <span className="mr-2">Sign In</span>{" "}
+      <ArrowRightIcon className="w-5 md:w-6" />
+    </Button>
+  );
 };
 
 export const RegisterButton = () => {
-    return (
-        <Link href="/register" style={{ marginRight: 10 }}>
-            Register
-        </Link>
-    );
+  return (
+    <Link href="/register" style={{ marginRight: 10 }}>
+      Register
+    </Link>
+  );
 };
 
-export const LogoutButton = ({ children }: { children: React.ReactNode }) => {
-    return (
-        <button className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-slate-100 dark:bg-slate-800 p-3 text-sm font-medium hover:bg-sky-200 dark:hover:bg-sky-950 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3" onClick={() => signOut()}>
-            {children}
-            <span className="mr-2 text-md text-bold">Sign out</span>
-        </button>
-    );
+export const LogoutButton = ({
+  className = "",
+  onClick = () => signOut(),
+}: {
+  className?: string;
+  onClick?: () => void;
+}) => {
+  return (
+    <button
+      className={`flex items-center gap-2 ${className}`}
+      onClick={onClick}
+    >
+      <PowerIcon className="w-6" />
+      <span>Sign out</span>
+    </button>
+  );
 };
 
 export const ProfileButton = () => {
-    return <Link href="/profile">Profile</Link>;
+  return <Link href="/profile">Profile</Link>;
 };
