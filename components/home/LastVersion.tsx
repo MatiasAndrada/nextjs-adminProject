98cfbc5395e32d0e66f8b2950e71038c7c61773f
Matias Andrada <matiasandrada5622@gmail.com>
2024-08-13 17:24:56 -0300

    changing rendering method for LastSession component and upgrade error handling. Min changes in styles.

--- components/home/LastVersion.tsx
+++ components/home/LastVersion.tsx
@@ -1,28 +1,59 @@
-import React from 'react'
+"use client";
+import { useEffect, useState } from "react";
 
-async function LastVersion() {
-    const response = await fetch('https://api.github.com/repos/matiasandrada/nextjs-adminProject/commits').then(res => res.json()).catch(err => console.error(err))
-    const lastCommitAuthor = response[0]?.commit.author.name
-    const lastCommitDate = response[0]?.commit.author.date
-    const transformDate = new Date(lastCommitDate).toLocaleDateString(undefined, { day: 'numeric', month: 'long', year: 'numeric' })
-    if (!response) return <div>loading...</div>
-    if (!lastCommitAuthor && lastCommitDate === undefined) {
-        const resMessage = response.message
-        const extractIP = resMessage?.split(' ')[5]
-        return (
-            <div className='text-gray-500 dark:text-slate-300 text-sm opacity-60'>
-                <h4 className='w-36 text-balance'>Exceded IP limit request in API github</h4>
-                <p>IP: {extractIP}</p>
-            </div>
-        )
+async function fetchLastCommit() {
+  try {
+    const response = await fetch(
+      "https://api.github.com/repos/matiasandrada/nextjs-adminProject/commits/master"
+    );
+    if (!response.ok) {
+      throw new Error("Failed to fetch data");
     }
-    return (
-        <div className='text-gray-500 dark:text-slate-300 text-sm opacity-60'>
-            <h4>Last update:</h4>
-            <p>Author: {lastCommitAuthor}</p>
-            <p>Date: {transformDate}</p>
-        </div>
-    )
+    const data = await response.json();
+    return data.commit.author;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
 }
 
-export default LastVersion
+function LastVersion() {
+  const [commitInfo, setCommitInfo] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetchLastCommit()
+      .then((data) => setCommitInfo(data))
+      .catch((error) => setError(error.message));
+  }, []);
+
+  if (error) {
+    return (
+      <div className="text-gray-500 dark:text-slate-300 text-sm opacity-60">
+        <h4 className="w-36 text-balance">Error fetching data</h4>
+        <p>{error}</p>
+      </div>
+    );
+  }
+
+  if (!commitInfo) {
+    return <div className="text-gray-500">Loading...</div>;
+  }
+
+  const { name: author, date } = commitInfo;
+  const formattedDate = new Date(date).toLocaleDateString(undefined, {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+
+  return (
+    <div className="text-gray-500 dark:text-slate-300 text-sm opacity-60">
+      <h4>Last update:</h4>
+      <p>Author: {author}</p>
+      <p>Date: {formattedDate}</p>
+    </div>
+  );
+}
+
+export default LastVersion;
--- components/home/StatusSession.tsx
+++ components/home/StatusSession.tsx
@@ -1,19 +1,18 @@
 import { SessionClient } from "@/components/home/SessionClient";
 import { SessionServer } from "@/components/home/SessionServer";
 
-
 export function StatusSession() {
-    return (
-        <div className="opacity-60">
-            <h4 className="text-gray-500 dark:text-slate-300 text-balance text-sm w-36">
-                Verify the server and client session status:
-            </h4>
-            <div className="flex flex-row gap-2">
-                <span className="text-xs">Server:</span>
-                <SessionServer />
-                <span className="text-xs">Client:</span>
-                <SessionClient />
-            </div>
-        </div >
-    );
+  return (
+    <div className="opacity-60 space-y-1 text-gray-500 dark:text-slate-300">
+      <h4 className="text-balance text-sm w-36">
+        Verify the server and client session status:
+      </h4>
+      <div className="flex flex-row gap-2">
+        <span className="text-xs">Server:</span>
+        <SessionServer />
+        <span className="text-xs ">Client:</span>
+        <SessionClient />
+      </div>
+    </div>
+  );
 }
